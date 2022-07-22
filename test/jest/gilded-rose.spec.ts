d5895ca2b3ac4ed67cb9f45217c729ebdf7c85bf
Catal <Tugba.Catal@afiniti.com>
2022-07-23 00:34:20 +0300

    create test cases between breakpoints

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,46 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
+	// credit: https://www.youtube.com/watch?v=2q5PdGdlL8Y - How to test legacy code with characterization tests
+	it('Generate expected tests', () => {
+		const names = [
+			'Aged Brie',
+			'Backstage passes to a TAFKAL80ETC concert',
+			'Sulfuras, Hand of Ragnaros',
+			'Normal Item'
+		];
+		// ranges of breaking points
+		const [minSellIn, maxSellIn] = [-1, 12];
+		const [minQuality, maxQuality] = [-1, 51];
+		// save all cases
+		const tests: Array<Array<string | number>> = [];
+
+		for (const name of names) {
+			for (let sellIn = minSellIn; sellIn <= maxSellIn; sellIn++) {
+				for (let quality = minQuality; quality <= maxQuality; quality++) {
+					const gildedRose = new GildedRose([new Item(name, sellIn, quality)]);
+					const items = gildedRose.updateQuality();
+					const outputSellIn = items[0].sellIn;
+					const outputQuality = items[0].quality;
+					tests.push([
+						name,
+						sellIn,
+						quality,
+						outputSellIn,
+						outputQuality
+					]);
+				}
+			}
+		}
+
+		console.log(JSON.stringify(tests));
+		console.log(tests.length);
+
+	});
+
+	it('should foo', () => {
+		const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+		const items = gildedRose.updateQuality();
+		expect(items[0].name).toBe('fixme');
+	});
 });
